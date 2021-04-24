34304d009826316d88dfd971dfdb7e6d8c24c48f
MarcoYu <marco.yu1995@gmail.com>
2021-04-24 15:13:39 -0300

    add valida input vacios

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded',function(){//DOMContentLoaded no ej
     const btn = document.getElementById('add');//obtengo el boton con el id add; variable btn tiene una referencia al boton; const no puedo modificarla ya que es constante
     const title = document.getElementById('title');//obtengo el input title
     const description = document.getElementById('description');//obtengo el input description
+    const table = document.getElementById('table');//obtengo la tabla
 
-    btn.onclick = function(){
-        console.log('Title:',title.value);
-        console.log('Description:',description.value);
-    };
+    function addTodo(){
+        if(title.value === '' || description.value === ''){ // title.value == '' error ya que 2 == '2' da verdadero; usar 2 === '2' ademas compara el tipo de dato
+            console.error('Title and description are required');
+        } else{
+            console.log('ok');
+        }
+    }
+    
+    btn.onclick = addTodo;
 });
 //DOM document object model, representa el html en forma de objetos para poder usarlos con javascript 
